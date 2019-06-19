f8b46b3c14acd7092a615bfa9844cf5ee87ec983
Christopher Dent <christopherdent01@gmail.com>
2019-06-19 00:52:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,7 +13,6 @@ introduction_with_language("Josh", "React")
 
 def introduction_with_language_optional(name, 'language')
       puts "Hi, my name is #{name} and I am learning to program in Ruby."
-    or
-      puts "Hi, my name is #{name} and I am learning to program in #{language}."
+    
 end
-introduction_with_language_optional("Edwin")
+introduction_with_language_optional("Edwin", "Python")
